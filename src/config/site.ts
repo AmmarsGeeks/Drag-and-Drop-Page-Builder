43d2ba7a860ebe9bd939cfc350067eaaf1bce851
Ammar Hammouch <codingammarhamdi@gmail.com>
2025-08-10 01:57:37 +0300

    adding layout for dashboard

---
+++ src/config/site.ts
@@ -0,0 +1,8 @@
+import type { Metadata } from "next"
+
+export const siteConfig = {
+  title: "AHammouch",
+  description:
+    "Built with love by: ahammouch.xyz",
+  metadataBase: new URL("https://ahammouch.xyz"),
+} satisfies Metadata
